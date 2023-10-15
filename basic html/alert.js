c8ac74d57ce31f3671c9e95a14652424f978aa0f
shashank R S <139559811+Shashankrs989@users.noreply.github.com>
2023-10-15 21:22:23 +0530

    Add files via upload

---
+++ basic html/alert.js
@@ -0,0 +1,8 @@
+var name = "SHASHANK";
+var age = 20;
+var message = "Hello, This is " + name +"   wellcome!! 😄😄if your age is above "+age +"\nclick ok";
+
+alert(message);
+function func(){
+    window.open("index2.html")
+}
