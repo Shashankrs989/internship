c7f8ce5350d072758cd28bf6900e58d741174c2d
shashank R S <139559811+Shashankrs989@users.noreply.github.com>
2023-11-28 16:13:25 +0530

    Delete basic html directory

--- basic html/alert.js
+++
@@ -1,8 +0,0 @@
-var name = "SHASHANK";
-var age = 20;
-var message = "Hello, This is " + name +"   wellcome!! 😄😄if your age is above "+age +"\nclick ok";
-
-alert(message);
-function func(){
-    window.open("index2.html")
-}
